d701c6ea5744f850330dbcc84b942c483d14ba76
btdDucBui <146934837+btdDucBui@users.noreply.github.com>
2024-03-01 00:39:55 +0700

    Add files via upload
    update code main.py for uart

--- Lab2/main.py
+++ Lab2/main.py
@@ -3,10 +3,11 @@ from Adafruit_IO import MQTTClient
 import time
 import random
 from simple_ai import *
+from uart import *
 
 AIO_FEED_ID = ["nutnhan1", "nutnhan2"]
 AIO_USERNAME = "Bui_Duc"
-AIO_KEY = "aio_zgoU88oCQ1sXRnlM6RBlieeaq5Jq"
+AIO_KEY = "aio_XQyV207m6GHRYaUSINSgOux8MPxx"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -37,35 +38,35 @@ ai_result = ""
 previous_result = ""
 
 while True:
-    counter = counter -1
-    if counter <= 0:
-        counter = 10
-        #TODO
-        print("Random data is publishing...")
-        if sensor_type == 0:
-            print("Temperature...")
-            temp = random.randint(10,20)
-            client.publish("cambien1",temp)
-            sensor_type = 1
-        elif sensor_type == 1:
-            print("Light...")
-            light = random.randint(100, 500)
-            client.publish("cambien2", light)
-            sensor_type = 2
-        elif sensor_type == 2:
-            print("Humidity..")
-            humi = random.randint(50, 70)
-            client.publish("cambien3", humi)
-            sensor_type = 0
+    # counter = counter -1
+    # if counter <= 0:
+    #     counter = 10
+    #     #TODO
+    #     print("Random data is publishing...")
+    #     if sensor_type == 0:
+    #         print("Temperature...")
+    #         temp = random.randint(10,20)
+    #         client.publish("cambien1",temp)
+    #         sensor_type = 1
+    #     elif sensor_type == 1:
+    #         print("Light...")
+    #         light = random.randint(100, 500)
+    #         client.publish("cambien2", light)
+    #         sensor_type = 2
+    #     elif sensor_type == 2:
+    #         print("Humidity..")
+    #         humi = random.randint(50, 70)
+    #         client.publish("cambien3", humi)
+    #         sensor_type = 0
 
-    counter_ai = counter_ai - 1
-    if counter_ai <= 0:
-        counter_ai = 5
-        previous_result = ai_result
-        ai_result= image_detector()
-        print("AI Output: ", ai_result)
-        if previous_result != ai_result:
-            client.publish("ai", ai_result)
-
+    # counter_ai = counter_ai - 1
+    # if counter_ai <= 0:
+    #     counter_ai = 5
+    #     previous_result = ai_result
+    #     ai_result= image_detector()
+    #     print("AI Output: ", ai_result)
+    #     if previous_result != ai_result:
+    #         client.publish("ai", ai_result)
+    readSerial(client)
     time.sleep(1)
     pass
